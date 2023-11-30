25d12b0b4b5ce989bcf39a7a0b2a6b8f10ad0bf6
Kendrick Oppong <kendrickoppong94@gmail.com>
2023-11-30 14:44:48 +0000

    initialized git and install sanity

---
+++ sanity/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: '95q7msu3',
+    dataset: 'production'
+  }
+})
---
+++ sanity/schemas/product.ts
@@ -0,0 +1,9 @@
+export default {
+  name: 'product',
+  title: 'Product',
+  type: 'document',
+  fields: [
+    {name: 'name', type: 'string', title: 'Name of Products'},
+    {name: 'images', type: 'array', title: 'Product Images'},
+  ],
+}

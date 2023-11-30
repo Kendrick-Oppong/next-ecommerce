09028fd359edd433b5b32d1783da75eb51413def
Kendrick Oppong <kendrickoppong94@gmail.com>
2023-11-30 15:29:39 +0000

    create  schema for socket io

--- sanity/schemas/product.ts
+++ sanity/schemas/product.ts
@@ -4,6 +4,10 @@ export default {
   type: 'document',
   fields: [
     {name: 'name', type: 'string', title: 'Name of Products'},
-    {name: 'images', type: 'array', title: 'Product Images'},
+    {name: 'images', type: 'array', of: [{type: 'image'}], title: 'Product Images'},
+    {name: 'description', type: 'text', title: 'Description of product'},
+    {name: 'slug', type: 'slug', title: 'Product slug', options: {source: 'name'}},
+    {name: 'price', type: 'number', title: 'Price'},
+    {name: 'category', title: 'Product category', type: 'reference', to: [{type:"category"}]},
   ],
 }

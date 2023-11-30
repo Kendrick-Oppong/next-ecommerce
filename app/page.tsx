3be2628af726d778672d9a0ed836aa65264057cf
Kendrick Oppong <kendrickoppong94@gmail.com>
2023-11-30 23:15:43 +0000

    fetch products

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Hero from "@/components/Hero";
-import Image from "next/image";
+import Newest from "@/components/Newest";
+
 
 export default function Home() {
   return (
     <main className="">
       <Hero />
+      <Newest/>
     </main>
   );
 }
